637dd02ae4696b38f60687bf3d3355ca4a31c58c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-15 12:44:41 +0100

    added filesystem fs module

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+import fs from "fs";
+
 console.log("Hello world");
+fs.readFile('example_log.txt', function (err, logData) {
+
+    // Если возникла ошибка, мы кидаем исключение
+    // и программа заканчивается
+    if (err) throw err;
+
+    // logData это объект типа Buffer, переводим в строку
+    let text = logData.toString();
+});
